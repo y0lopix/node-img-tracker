8ed5103acaa93e8a6467846ef0e76edf3c5fd03f
Yolopix <13918281+y0lopix@users.noreply.github.com>
2020-11-12 11:51:48 +0100

    init

---
+++ main.js
@@ -0,0 +1,16 @@
+const http = require("http");
+const host = 'localhost';
+const port = 80;
+
+const requestListener = function (req, res) {
+    res.setHeader("Content-Type", "image/png");
+    res.writeHead(200);
+    res.end(Buffer.from("iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR4nGP6zwAAAgcBApocMXEAAAAASUVORK5CYII=", 'base64'));
+    console.log("\n" + new Date().toISOString() + " - " + req.connection.remoteAddress + " - " + req.url);
+    console.log(req.headers);
+};
+
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+    console.log("Serveur démarré");
+});
